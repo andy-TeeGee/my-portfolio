3078bdf8a90880c83f06776eb6d4308ee141a525
Andrew <andrewtgoheen@gmail.com>
2026-01-10 19:28:53 -0500

    GA4 tags set for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Montserrat } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import { Header } from "@/components/header"
 import Footer from "@/components/footer"
@@ -24,6 +25,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-14FWMYHH0C"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-14FUMYHM0C');
+          `}
+        </Script>
+
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
